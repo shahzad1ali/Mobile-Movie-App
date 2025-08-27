a5c5d6543fc957fcdf9cde11da129d2e13e622c8
shahzad1ali <shahzadmobi619@gmail.com>
2025-08-27 11:52:54 +0500

    first project of react native

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from 'react-native';
+
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+  value?: string;
+ onChangeText?: (text: string) => void;
+
+}
+
+const SearchBar = ({placeholder, onPress, value,onChangeText}: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#abb8ff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#a8b5ff"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
---
+++ app/components/TrendingCard.tsx
@@ -0,0 +1,41 @@
+import { images } from "@/constants/images";
+import MaskedView from '@react-native-masked-view/masked-view';
+import { Text } from "@react-navigation/elements";
+import { Link } from "expo-router";
+import React from 'react';
+import { Image, TouchableOpacity, View } from "react-native";
+
+const TrendingCard = ({ movie, index }: { movie: any; index: number }) => {
+  return (
+    <Link href={`/movies/${movie.id}`} asChild>
+      <TouchableOpacity className="w-32 relative pl-5">
+        <Image
+          source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+          className="w-32 h-48 rounded-lg"
+          resizeMode="cover"
+        />
+
+<View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+  <MaskedView
+    maskElement={
+      <Text className="font-bold text-white text-6xl">
+        {index + 1}
+      </Text>
+    }
+  >
+    <Image
+      source={images.rankingGradient}
+      className="size-14"
+      resizeMode="cover"
+    />
+  </MaskedView>
+</View>
+
+<Text className="text-sm font-bold text-light-200 mt-2" numberOfLines={2}>{movie.title}</Text>
+
+      </TouchableOpacity>
+    </Link> 
+  )
+}
+
+export default TrendingCard;
